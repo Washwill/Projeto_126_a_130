cd8cfca3818ae7eb33a91d8bbec49b1224816f5b
Washwill <104589724+Washwill@users.noreply.github.com>
2023-03-02 14:51:56 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 musica1 = "";
 musica2 = "";
+pulsoEsquerdoY = 0;
+pulsoEsquerdoX = 0;
+pulsoDireitoY = 0;
+pulsoDireitoX = 0;
+
 
 function preload(){
     musica1 = loadSound("music.mp3");
@@ -11,6 +16,22 @@ function setup(){
     canvas.position(400, 230);
     video = createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        pulsoEsquerdoX = results[0].pose.leftWrist.x;
+        pulsoEsquerdoY = results[0].pose.leftWrist.y;
+        pulsoDireitoX = results[0].pose.rightWrist.x;
+        pulsoDireitoY = results[0].pose.rightWrist.y;
+    }
+}
+
+function modelLoaded(){
+    console.log("poseNet carregado");
 }
 
 function draw(){
