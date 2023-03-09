1740b51b477098fbc077771e70a7b38d5a03eb23
Washwill <104589724+Washwill@users.noreply.github.com>
2023-03-09 17:39:48 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-musica1 = "";
-musica2 = "";
+var musica1 = "";
+var musica2 = "";
+pulsoEsquerdo = 0;
+pulsoDireito = 0;
 pulsoEsquerdoY = 0;
 pulsoEsquerdoX = 0;
 pulsoDireitoY = 0;
@@ -23,10 +25,17 @@ function setup(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        pulsoDireito = results[0].pose.keypoints[10].score;
+        pulsoEsquerdo = results[0].pose.keypoints[9].score;
+        console.log("pulsoEsquerdo = " + pulsoEsquerdo);
+
         pulsoEsquerdoX = results[0].pose.leftWrist.x;
         pulsoEsquerdoY = results[0].pose.leftWrist.y;
+        console.log("pulsoEsquerdoX = " + pulsoEsquerdoX + "pulsoEsquerdoY = " + pulsoEsquerdoY);
+
         pulsoDireitoX = results[0].pose.rightWrist.x;
         pulsoDireitoY = results[0].pose.rightWrist.y;
+        console.log("pulsoDireitoX = " + pulsoDireitoX + "pulsoDireitoY = " + pulsoDireitoY);
     }
 }
 
@@ -36,4 +45,44 @@ function modelLoaded(){
 
 function draw(){
     image(video, 0, 0, 600, 500);
+    if(pulsoEsquerdo > 0.2){
+        fill("green");
+        stroke("cyan");
+        circle(pulsoDireitoX, pulsoDireitoY, 20);
+        musica2.stop();
+        if(musica1 = false){
+            play2();
+            document.getElementById("nomeMusica").innerHTML = "Música do Harry Potter (Eu acho)";
+        }
+        
+    }
+    isPlaying();
+    if(pulsoDireito > 0.2){
+        fill("cyan");
+        stroke("green");
+        circle(pulsoEsquerdoX, pulsoEsquerdoY, 20);
+        musica1.stop();
+        if(musica2 = false){
+            play();
+            document.getElementById("nomeMusica").innerHTML = "Música do Peter Pan";
+        }
+    }
 }
+
+function isPlaying(){
+    musica1.isPlaying();
+}
+
+function play(){
+    musica1.play();
+    musica1.setVolume(1);
+    musica1.rate(1);
+}
+
+function play2(){
+    musica2.play2();
+    musica2.setVolume(1);
+    musica2.rate(1);
+}
+
+play();
